84c6bf3b0bdbe2ed46e0ca4d60eaeb56bb36dc11
madziag <gamba_m@yahoo.com>
2018-06-27 18:22:35 +0200

    First commit

---
+++ DataInsert.php
@@ -0,0 +1,38 @@
+<?php
+
+$checked1 = 'checked = \"checked\"'
+
+?>
+
+<html>
+<body>
+
+<form action= "<?php echo $action ?>"  method="post">
+
+First name: <input type="text" name="firstname" value="<?php echo $firstname ?>"><br />
+
+Last name: <input type="text" name="lastname" value="<?php echo $lastname ?>"><br />
+
+Street address: <input type="text" name="streetaddress" value="<?php echo $streetaddress ?>"><br />
+Postcode: <input type="text" name="postcode" value="<?php echo $postcode ?>"><br />
+Town: <input type="text" name="town" value="<?php echo $town ?>"><br />
+
+E-mail: <input type="text" name="email" value="<?php echo $email ?>"><br />
+
+Main Phone: <input type="text" name="mainphone" value="<?php echo $mainphone ?>"><br />
+Alt Phone: <input type="text" name="altphone" value="<?php echo $altphone ?>"><br />
+
+Status:
+<input type="radio" name="status"
+<?php if (isset($status) && $status=="0") echo $checked1;?>
+value="0">Active
+<input type="radio" name="status"
+<?php if (isset($status) && $status=="1") echo $checked1;?>
+value="1">Inactive <br />
+
+
+
+<input type="submit">
+</form>
+</body>
+</html>
---
+++ SearchRetrieve.php
@@ -0,0 +1,134 @@
+<?php
+
+ $servername = 'localhost';
+ $username = 'MadziaG';
+ $password = 'P$i@krew2018User';
+ $dbname = 'englishschooldb';
+
+
+ // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+     if ($conn->connect_error) {
+  	   die("Connection failed: " . $conn->connect_error);
+    }
+
+// To change this to an update page after the sql query
+
+$action = '\"helloworld.php\"';
+
+if(isset($_POST["firstname"])){
+	$firstname = $_POST["firstname"];
+}
+
+if(isset($_POST["lastname"])){
+	$lastname = $_POST["lastname"];
+}
+
+if(isset($_POST["streetaddress"])){
+	$streetaddress = $_POST["streetaddress"];
+}
+
+if(isset($_POST["postcode"])){
+	$postcode = $_POST["postcode"];
+}
+
+if(isset($_POST["town"])){
+	$town = $_POST["town"];
+}
+
+if(isset($_POST["email"])){
+	$email = $_POST["email"];
+}
+
+if(isset($_POST["mainphone"])){
+	$mainphone = $_POST["mainphone"];
+}
+
+if(isset($_POST["altphone"])){
+	$altphone = $_POST["altphone"];
+}
+
+if(isset($_POST["status"])){
+	$status = $_POST["status"];
+}
+
+
+// build sql query
+$sql = "Select * from students where ";
+		if (!empty($firstname) ){
+		$sql = $sql . "first_name = '" . $firstname . "'";
+		}
+		if (!empty($lastname) ){
+		$sql = $sql . " and last_name = '" . $lastname . "'";
+		}
+		if (!empty($streetadress) ){
+		$sql = $sql . " and street_address = '" . $streetadress . "'";
+		}
+		if (!empty($postcode) ){
+		$sql = $sql . " and address_code = '" . $postcode . "'";
+		}
+		if (!empty($town) ){
+		$sql = $sql . " and town = '" . $town . "'";
+		}
+		if (!empty($mainphone) ){
+		$sql = $sql . " and phone_main = '" . $mainphone . "'";
+		}
+		if (!empty($altphone) ){
+		$sql = $sql . " and phone_alt = '" . $altphone . "'";
+		}
+		if (!empty($email) ){
+		$sql = $sql . " and email = '" . $email . "'";
+		}
+		if (!empty($status) ){
+		$sql = $sql . " and inactive = '" . $status . "'";
+		}
+		$sql = preg_replace('/where\s+and/','where', $sql);
+
+
+// Running query
+if($sql != "Select * from students where "){
+			$result = $conn->query($sql)
+	        or trigger_error($conn->error);
+			$row = $result->fetch_array(MYSQLI_BOTH);
+		}
+
+
+$num_rows = mysqli_num_rows($result);
+
+//reset vars based on SQL query
+
+if ($num_rows == 1){
+
+		$firstname = $row["first_name"];
+        $lastname = $row["last_name"];
+		$streetaddress = $row["street_address"];
+		$postcode = $row["address_code"];
+		$town = $row["town"];
+		$email = $row["email"];
+		$mainphone = $row["phone_main"];
+		$altphone = $row["phone_alt"];
+		$status = $row["inactive"];
+
+		// $action = '\"Dataupdate.php\"';
+
+}
+
+// More than one result -- needs to be worked on
+if ($num_rows > 1){
+echo "foo";
+}
+
+
+
+
+
+
+
+include 'DataInsert.php';
+
+
+$conn->close();
+
+?>
+
