e97e58601f84ec1f43b3384155b7c0e00df1ed15
madziag <gamba_m@yahoo.com>
2018-10-23 22:06:56 +0200

    Update SearchRetrieve.php

--- SearchRetrieve.php
+++ SearchRetrieve.php
@@ -1,10 +1,5 @@
 <?php
 
- $servername = 'localhost';
- $username = 'MadziaG';
- $password = 'P$i@krew2018User';
- $dbname = 'englishschooldb';
-
 
  // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
