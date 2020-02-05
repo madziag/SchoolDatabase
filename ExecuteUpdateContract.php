2746ba1f9b78cac9e31b624ac11ab0a9a8ad1f51
madziag <gamba_m@yahoo.com>
2020-02-05 10:31:50 +0100

    Update ExecuteUpdateContract.php

--- ExecuteUpdateContract.php
+++ ExecuteUpdateContract.php
@@ -1,8 +1,8 @@
 <?php
 
  $servername = 'localhost';
- $username = 'MadziaG';
- $password = 'P$i@krew2018User';
+ $username = ;
+ $password = ;
  $dbname = 'englishschooldb';
 
  // Create connection
